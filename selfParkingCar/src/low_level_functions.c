c8590ff090edf26b0bde01eabe0da669acc0b308
Salma-nasser <133383515+Salma-nasser@users.noreply.github.com>
2025-05-07 01:45:51 +0300

    updated with low level function and sensor config

---
+++ selfParkingCar/src/low_level_functions.c
@@ -0,0 +1,52 @@
+#include "low_level_functions.h"
+#include "gpio_defs.h"
+#include "esp32/rom/ets_sys.h"  // for ets_delay_us
+#include "freertos/FreeRTOS.h"  // for portTICK_PERIOD_MS
+
+void pinConfig(uint8_t pin, uint8_t mode) {
+    if (mode == OUTPUT) {
+        if (pin < 32) {
+            *((volatile uint32_t *)GPIO_ENABLE_W1TS_REG) = (1 << pin);
+        } else {
+            *((volatile uint32_t *)GPIO_ENABLE1_W1TS_REG) = (1 << (pin - 32));
+        }
+    } else if (mode == INPUT) {
+        if (pin < 32) {
+            *((volatile uint32_t *)GPIO_ENABLE_W1TC_REG) = (1 << pin);
+        } else {
+            *((volatile uint32_t *)GPIO_ENABLE1_W1TC_REG) = (1 << (pin - 32));
+        }
+    }
+}
+
+void digitalWriteLowLevel(uint8_t pin, uint8_t value) {
+    if (value) {
+        if (pin < 32) {
+            *((volatile uint32_t *)GPIO_OUT_W1TS_REG) = (1 << pin);
+        } else {
+            *((volatile uint32_t *)GPIO_OUT1_W1TS_REG) = (1 << (pin - 32));
+        }
+    } else {
+        if (pin < 32) {
+            *((volatile uint32_t *)GPIO_OUT_W1TC_REG) = (1 << pin);
+        } else {
+            *((volatile uint32_t *)GPIO_OUT1_W1TC_REG) = (1 << (pin - 32));
+        }
+    }
+}
+
+int digitalReadLowLevel(uint8_t pin) {
+    if (pin < 32) {
+        return ((*((volatile uint32_t *)GPIO_IN_REG)) >> pin) & 1;
+    } else {
+        return ((*((volatile uint32_t *)GPIO_IN1_REG)) >> (pin - 32)) & 1;
+    }
+}
+
+void custDelay(uint32_t ms) {
+    vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
+void custDelayMicroseconds(uint32_t us) {
+    ets_delay_us(us);
+}
--- selfParkingCar/src/main.c
+++ selfParkingCar/src/main.c
@@ -1,1 +1,33 @@
-void app_main() {}
+#include <stdio.h>
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ultrasonic.h"
+
+#define TRIG_FRONT 5
+#define ECHO_FRONT 18
+#define TRIG_BACK 2
+#define ECHO_BACK 15
+#define TRIG_LEFT 32
+#define ECHO_LEFT 33
+#define TRIG_RIGHT 12
+#define ECHO_RIGHT 13
+
+void app_main() {
+  init_sensor(TRIG_FRONT, ECHO_FRONT);
+  init_sensor(TRIG_BACK, ECHO_BACK);
+  init_sensor(TRIG_LEFT, ECHO_LEFT);
+  init_sensor(TRIG_RIGHT, ECHO_RIGHT);
+
+  while (1) {
+    float front = read_distance(TRIG_FRONT, ECHO_FRONT);
+    float back = read_distance(TRIG_BACK, ECHO_BACK);
+    float left = read_distance(TRIG_LEFT, ECHO_LEFT);
+    float right = read_distance(TRIG_RIGHT, ECHO_RIGHT);
+
+    ESP_LOGI("DISTANCES", "Front: %.2f, Back: %.2f, Left: %.2f, Right: %.2f", front, back, left, right);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
